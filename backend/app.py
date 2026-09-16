87a43f1e717ff16f7afaeb48b7b57cb6e57fa8e1
ramanikantasingha-art <ramanikantasingha@gmail.com>
2026-09-16 13:33:34 +0530

    Allow both Windows local hostnames

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,12 @@ from rembg import remove
 app = FastAPI(title="Natural Passport Photo Enhancer API", version="2.0.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
